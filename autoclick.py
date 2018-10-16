fdf6080c251c43d29af5c1ea9235ab27617d7ad2
Tianjie Yang <tianjyan@trackview.co>
2018-10-16 14:57:55 +0800

    Update

--- autoclick.py
+++ autoclick.py
@@ -21,16 +21,32 @@ class AutoClick:
             self.revisit(0, 500)
 
     def revisit(self, x, y):
+        # 点击选择第一个客户
+        self.tap(x, y)
+        # 等待客户信息加载完成
+        time.sleep(3)
+        # 点击新增跟进
+        self.tap(1000, 1800)
+        # 等待新增跟进页面加载完成
+        time.sleep(3)
+        # 选择跟进方式为去电
+        self.tap(420, 350)
+        # 点击跟进内容图标
+        self.tap(900, 1100)
+        # 点击确认按钮
+        self.tap(1020, 1150)
+        # 点击保存
+        self.tap(1000, 1800)
+        # 回退到客户列表
+        self.goback()
+        # 等待页面刷新完成
+        time.sleep(3)
+        
+    def tap(self, x, y):
         os.system(u'adb shell input tap {} {}'.format(x, y))
-        time.sleep(3)
-        os.system(u'adb shell input tap {} {}'.format(1000, 1800))
-        time.sleep(3)
-        os.system(u'adb shell input tap {} {}'.format(420, 350))
-        os.system(u'adb shell input tap {} {}'.format(900, 1100))
-        os.system(u'adb shell input tap {} {}'.format(1020, 1150))
-        os.system(u'adb shell input tap {} {}'.format(1000, 1800))
+
+    def goback(self):
         os.system(u'adb shell input keyevent 4')
-        time.sleep(3)
 
 if __name__ == "__main__":
     logger = Logger()
